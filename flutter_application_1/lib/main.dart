ad744fb0e83808327c19317f136f4a5273f90cf2
Glenn <fallwich@naver.com>
2022-11-03 22:36:09 +0900

    UI 연습
    UI 연습

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,58 +6,231 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'sorry'),
+      home: const MyHomePage(title: 'test'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String _counter = "hi";
+  int _counter = 0;
+  double sideLength = 50;
+  double sideLength2 = 50;
+  double sideLength3 = 50;
+  double sideLength4 = 50;
   void _incrementCounter() {
     setState(() {
-      _counter += "@";
+      _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Container(
+                  height: 300,
+                  color: Color.fromARGB(255, 152, 197, 235),
+                  child: Row(children: [
+                    Container(
+                      height: 300,
+                      width: 50,
+                    ),
+                    InkWell(
+                      child: Image.asset(
+                        "assets/images/home.png",
+                        height: sideLength,
+                        width: sideLength,
+                      ),
+                      onTap: () {
+                        setState(() {
+                          if (sideLength == 50) {
+                            sideLength = 100;
+                          }
+                        });
+                        print(" on tap ");
+                      },
+                      onDoubleTap: () {
+                        setState(() {
+                          if (sideLength == 100) {
+                            sideLength = 50;
+                          }
+                        });
+                        print(" on Double Tap ");
+                      },
+                    ),
+                    Container(
+                      height: 300,
+                      width: 70,
+                    ),
+                    Expanded(
+                      child: Text("Online Shopping",
+                          style: TextStyle(fontSize: 30, color: Colors.white)),
+                    )
+                  ])),
             ),
-            Text(
-              _counter,
-              style: Theme.of(context).textTheme.headline4,
+            Flexible(
+                child: Container(
+                    height: 300,
+                    color: Colors.lightBlueAccent,
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 300,
+                          width: 50,
+                        ),
+                        InkWell(
+                          child: Image.asset(
+                            "assets/images/bucket.png",
+                            height: sideLength2,
+                            width: sideLength2,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              if (sideLength2 == 50) {
+                                sideLength2 = 100;
+                              }
+                            });
+                            print(" on tap ");
+                          },
+                          onDoubleTap: () {
+                            setState(() {
+                              if (sideLength2 == 100) {
+                                sideLength2 = 50;
+                              }
+                            });
+                            print(" on Double Tap ");
+                          },
+                        ),
+                        Container(
+                          height: 300,
+                          width: 70,
+                        ),
+                        Expanded(
+                          child: Text("Add to Cart",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        )
+                      ],
+                    ))),
+            Flexible(
+                child: Container(
+                    height: 300,
+                    color: Color.fromARGB(255, 46, 108, 167),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 300,
+                          width: 50,
+                        ),
+                        InkWell(
+                          child: Image.asset(
+                            "assets/images/card.png",
+                            height: sideLength3,
+                            width: sideLength3,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              if (sideLength3 == 50) {
+                                sideLength3 = 100;
+                              }
+                            });
+                            print(" on tap ");
+                          },
+                          onDoubleTap: () {
+                            setState(() {
+                              if (sideLength3 == 100) {
+                                sideLength3 = 50;
+                              }
+                            });
+                            print(" on Double Tap ");
+                          },
+                        ),
+                        Container(
+                          height: 300,
+                          width: 70,
+                        ),
+                        Expanded(
+                          child: Text("Secure Payments",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        )
+                      ],
+                    ))),
+            Flexible(
+                child: Container(
+                    height: 300,
+                    color: Color.fromARGB(255, 26, 80, 182),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: 300,
+                          width: 50,
+                        ),
+                        InkWell(
+                          child: Image.asset(
+                            "assets/images/truck.png",
+                            height: sideLength4,
+                            width: sideLength4,
+                          ),
+                          onTap: () {
+                            setState(() {
+                              if (sideLength4 == 50) {
+                                sideLength4 = 100;
+                              }
+                            });
+                            print(" on tap ");
+                          },
+                          onDoubleTap: () {
+                            setState(() {
+                              if (sideLength4 == 100) {
+                                sideLength4 = 50;
+                              }
+                            });
+                            print(" on Double Tap ");
+                          },
+                        ),
+                        Container(
+                          height: 300,
+                          width: 70,
+                        ),
+                        Expanded(
+                          child: Text("Fast Delivery",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        )
+                      ],
+                    ))),
+            Container(
+              height: 100,
+              width: 70,
             ),
           ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+        ));
   }
 }
+
+class Flexibla {}
